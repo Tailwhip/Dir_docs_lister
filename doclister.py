c317a61cb3c1db46d479b85bf4c6a19467782881
Mateusz Zdunek <zdunek.mateusz92@gmail.com>
2019-06-30 21:01:49 +0200

    added listing and grouping docs

---
+++ doclister.py
@@ -0,0 +1,26 @@
+import os
+from openpyxl.styles import Font
+
+def makelist(_path, _doclist, _level = 1):
+    for item in os.listdir(_path):
+        if os.path.isdir(_path + '/' + item):
+            #print("{} jest folderem".format(item))
+            _doclist.append([_path + '/', item, _level])
+            new_path = _path + '/' + item
+            makelist(new_path, _doclist, _level=_level+1)
+        else:
+            _doclist.append([_path + '/', item, _level])
+    return _doclist
+
+def exportlist(_doclist, _sheet,_start = 1):
+    style = Font(bold=True)
+    for doc in _doclist:
+        _sheet['A' + str(_start)] = doc[1]
+        if os.path.isdir(doc[0]+doc[1]):
+            print("{}-{} --> FOLDER".format(doc[1], doc[2]))
+            _sheet.row_dimensions.group(_start, _start, hidden=True, outline_level=doc[2])
+            _sheet['A' + str(_start)].font = style
+        else:
+            print("{}-{} --> plik".format(doc[1], doc[2]))
+            _sheet.row_dimensions.group(_start, _start, hidden=True, outline_level=doc[2])
+        _start += 1
--- main.py
+++ main.py
@@ -1,33 +1,14 @@
-import os
-import openpyxl
-from openpyxl import Workbook
-from openpyxl.utils import get_column_letter
 from openpyxl import load_workbook
 import kznumber as kzn
-import os
+import doclister
 
-
-dir_end = False
-path = 'E:/Zajecia/Projekty/14-Inteligentny_pojazd/01-Mechanika'
-# 'E:/Zajecia/Projekty/17-Dir_docs_lister/Dir_docs_lister'
-# 'E:/Zajecia/Projekty/14-Inteligentny_pojazd/01-Mechanika'
-
+# getting KZ number from user
 kz_number = kzn.kzNumber()
 
-def list(_path):
-    for item in os.listdir(_path):
-        if os.path.isdir(_path + '/' + item):
-            print("{} jest folderem".format(item))
-            new_path = _path + '/' + item
-            list(new_path)
-        else: print("{} jest plikiem".format(item))
+# setting a path for listing documents
+path = 'E:/Zajecia/Projekty/14-Inteligentny_pojazd/01-Mechanika'
+# 'E:/Zajecia/Projekty/17-Dir_docs_lister/Dir_docs_lister'
 
-list(path)
-
-'''
-for item in os.listdir(path):
-
-'''
 # destination filename
 dest_filename = kz_number.kz_I + '-Lista_dokumentów.xlsx'
 
@@ -35,10 +16,7 @@ wb = load_workbook(filename = 'XXX-Lista_dokumentów.xlsx')
 
 sheet = wb['Lista_dokumentów']
 
-sheet.row_dimensions.group(4, 15, hidden = True)
-
-sheet['A11'] = "Twoja stara"
-
-sheet.row_dimensions.group(11, 14, hidden = True, outline_level=2)
+# getting list of documents
+doclister.exportlist(doclister.makelist(path, _doclist = []), sheet, _start = 2)
 
 wb.save(dest_filename)
