23c52e0a8556ad656fb2749188bc2dc76ff35c96
Mateusz Zdunek <zdunek.mateusz92@gmail.com>
2019-07-03 21:54:28 +0200

    adding documents replacing mechanism

--- doclister.py
+++ doclister.py
@@ -1,15 +1,15 @@
 import os
 from openpyxl.styles import Font
 
-def makelist(_path, _doclist, _level = 1):
-    for item in os.listdir(_path):
-        if os.path.isdir(_path + '/' + item):
-            #print("{} jest folderem".format(item))
-            _doclist.append([_path + '/', item, _level])
-            new_path = _path + '/' + item
-            makelist(new_path, _doclist, _level=_level+1)
-        elif os.path.isfile(_path + '/' + item):
-            _doclist.append([_path + '/', item, _level])
+# making a list of documents where: _doclist[0] is a doc path, [1] is a doc name, [2] is a group level
+def makedoclist(_path, _doclist, _level = 1):
+    for name in os.listdir(_path):
+        if os.path.isdir(_path + '/' + name):
+            _doclist.append([_path + '/', name, _level])
+            new_path = _path + '/' + name
+            makedoclist(new_path, _doclist, _level=_level+1)
+        elif os.path.isfile(_path + '/' + name):
+            _doclist.append([_path + '/', name, _level])
     return _doclist
 
 def exportlist(_doclist, _sheet,_kznum, _start = 1):
@@ -17,13 +17,88 @@ def exportlist(_doclist, _sheet,_kznum, _start = 1):
     for doc in _doclist:
         _sheet['A' + str(_start)] = doc[1]
         if os.path.isdir(doc[0]+doc[1]):
-            print("{}-{} --> FOLDER".format(doc[1], doc[2]))
+            #print("{}-{} --> FOLDER".format(doc[1], doc[2]))
             _sheet.row_dimensions.group(_start, _start, hidden=True, outline_level=doc[2])
             _sheet['A' + str(_start)].font = style
         else:
-            print("{}-{} --> plik".format(doc[1], doc[2]))
+            #print("{}-{} --> plik".format(doc[1], doc[2]))
             _sheet.row_dimensions.group(_start, _start, hidden=True, outline_level=doc[2])
             if str(_sheet['B' + str(_start)].internal_value) == 'None':
                 _sheet['B' + str(_start)] = _kznum
             else: _sheet['B' + str(_start)] = str(_sheet['B' + str(_start)].internal_value) + ', ' + _kznum
         _start += 1
+    print("LIST IS DONE")
+
+def makelist(_doclist, _sheet, _start = 2):
+    while str(_sheet['A' + str(_start)].internal_value) != 'None':
+        _doclist.append(_sheet['A' + str(_start)].internal_value)
+        _start += 1
+    #for item in _doclist:
+     #   print(item)
+    print('OLD LIST DONE')
+    return _doclist
+
+def convtonamelist(_doclist):
+    newlist = []
+    for doc in _doclist:
+        newlist.append(doc[1])
+    #for item in newlist:
+     #   print(item)
+    print('NEW LIST DONE')
+    return newlist
+
+def evenlists(_oldlist, _newlist):
+    # make both list length even by adding blank values at the end of shorter one
+    if _oldlist.__len__() < _newlist.__len__():
+        for i in range(_newlist.__len__() - _oldlist.__len__()):
+            _oldlist.append('')
+    elif _oldlist.__len__() > _newlist.__len__():
+        for i in range(_oldlist.__len__() - _newlist.__len__()):
+            _newlist.append('')
+
+def wordtolist(word):
+    list = []
+    for letter in word:
+        list.append(letter)
+    return list
+
+def comparelists(_oldlist=[], _newlist=[]):
+    evenlists(_oldlist, _newlist)
+    letters_limit = 4
+    # comare document lists
+    for i, doc in enumerate(_newlist):
+        print(str(i) + '. OLD ' + _oldlist[i])
+        print('                                                        ' + str(i) + '. NEW ' + doc)
+        # if document in one list is the same as in second then do nothing
+        # but if not then compare letter after letter and check how many consecutive letters match
+        if doc == _oldlist[i]:
+            pass
+        else:
+            # convert each document name to list and even the lists length
+            olddoc = wordtolist(_oldlist[i])
+            doc = wordtolist(doc)
+            evenlists(olddoc, doc)
+            '''
+            for letter in olddoc:
+                print(letter)
+
+            for letter in doc:
+                print('                          ' + letter)'''
+            letters_counter = 0
+            matching_letter = True
+            # counting consecutive letters
+            for j, letter in enumerate(doc):
+                if letter == olddoc[j] and matching_letter == True:
+                    letters_counter += 1
+                else:
+                    matching_letter = False
+
+            if letters_counter > letters_limit:
+
+
+            print(str(i) + '. matching letters: ' + str(letters_counter))
+            break
+
+
+
+
--- main.py
+++ main.py
@@ -1,23 +1,59 @@
 from openpyxl import load_workbook
 import kznumber as kzn
 import doclister
+import os
 
 # getting KZ number from user
 kz_number = kzn.kzNumber()
 
 # setting a path for listing documents
-path = 'C:/Repozytorium/Approved/031-Pxx0/W031-Pxx0'
-#'E:/Zajecia/Projekty/14-Inteligentny_pojazd/01-Mechanika'
+project_path = 'E:/Zajecia/Projekty/14-Inteligentny_pojazd/01-Mechanika'
+'C:/Repozytorium/Approved/031-Pxx0/W031-Pxx0'
+# 'E:/Zajecia/Projekty/14-Inteligentny_pojazd/01-Mechanika'
 # 'E:/Zajecia/Projekty/17-Dir_docs_lister/Dir_docs_lister'
 
 # destination filename
-dest_filename = 'W' + kz_number.kz_I + '-00-Spis_dokumentacji_wyrobu.xlsx'
+list_name = 'W' + kz_number.kz_I + '-00-Spis_dokumentacji_wyrobu.xlsx'
 
-wb = load_workbook(filename = 'XXX-Szablon_listy.xlsx')
+list_path = project_path + '/' + list_name
 
-sheet = wb['Lista_dokumentów']
+# number of row where the list is beginning
+begin = 2
 
-# getting list of documents
-doclister.exportlist(doclister.makelist(path, _doclist = []), sheet, kz_number.kz, _start = 2)
+# check if a list already exist
+if os.path.isfile(list_path):
+    # if exist - update a file using KZ number
+    print("Update a file")
 
-wb.save(dest_filename)
+    wb = load_workbook(filename=list_path)
+    sheet = wb['Lista_dokumentów']
+
+    # making existing list of documents for comparison
+    existing_list = doclister.makelist([], sheet, begin)
+
+    # making list of new documents for comparison
+    new_list = doclister.convtonamelist(doclister.makedoclist(project_path, []))
+
+    doclister.comparelists(existing_list, new_list)
+
+    #for item in new_list:
+     #   print(item)
+
+
+    # updating list of documents
+    doclister.exportlist(doclister.makedoclist(project_path, _doclist=[]), sheet, kz_number.kz, begin)
+
+    wb.save(list_path)
+else:
+    # if not - make one using KZ number
+    print("Make a file")
+
+    wb = load_workbook(filename='XXX-Szablon_listy.xlsx')
+    sheet = wb['Lista_dokumentów']
+
+    # getting list of documents
+    doclister.exportlist(doclister.makedoclist(project_path, _doclist=[]), sheet, kz_number.kz, begin)
+
+    wb.save(list_path)
+
+
